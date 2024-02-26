c8f2665dc36c79e0c5aeb50f8d13dcaf81e4533c
camisbersann <camila.bersan@aluno.senai.br>
2024-02-26 16:46:20 -0300

    navigation

--- App.jsx
+++ App.jsx
@@ -1,20 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+//Responsável por gerenciar a navegação;
+import { NavigationContainer } from "@react-navigation/native";
+
+//Responsável por criar a navegação em pilha;
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import { StatusBar } from "expo-status-bar";
+
+import Home from "./src/screens/Home";
+import Profile from "./src/screens/Profile";
+import Contact from "./src/screens/Contact";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const Stack = createNativeStackNavigator();
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Profile" component={Profile} />
+        <Stack.Screen name="Contact" component={Contact} />
+      </Stack.Navigator>
+      <StatusBar style="auto" />
+    </NavigationContainer>
+  )
+}
---
+++ src/screens/Home/index.jsx
@@ -0,0 +1,21 @@
+import { View, Text, TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import styles from './styles';
+
+export default function Home() {
+    const navigation = useNavigation();
+
+  return (
+    <View style= {styles.container}>
+      <Text>Home</Text>
+      <TouchableOpacity style={styles.navButton} onPress={() => navigation.navigate("Profile")}>
+        <Text>Profile</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.navButton} onPress={() => navigation.navigate("Contact")}>
+        <Text>Contact</Text>
+      </TouchableOpacity>
+      
+    </View>
+  )
+}
